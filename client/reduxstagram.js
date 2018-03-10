0268dfa7bf23eb55a9ddec64395ffc80fd49d306
Eric <theericlau@gmail.com>
2018-03-09 22:29:01 -0800

    Working routing between components

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -5,5 +5,19 @@ import { render } from 'react-dom';
 //Import css
 import css from './styles/style.styl';
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main/>, document.getElementById('root'));
+//Import react router dependencies
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
