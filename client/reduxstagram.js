ca9e2819e915b6376c3fc5023ebb7890eaf4d0f9
Eric <theericlau@gmail.com>
2018-03-09 20:26:34 -0800

    init the component

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+//Import css
+import css from './styles/style.styl';
+
+render(<p>hi</p>, document.getElementById('root'));
