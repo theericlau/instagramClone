553fcf6897f09e84d7efb47b1e135b16e237cdc2
Eric <theericlau@gmail.com>
2018-03-10 15:35:51 -0800

    created reducer for posts

---
+++ client/reducers/posts.js
@@ -0,0 +1,10 @@
+// a reducer takes in two things:
+
+//actions (information about what happened)
+//copy of current state
+
+function posts(state = [], action) {
+  console.log(state, action);
+}
+
+export default posts;
