643be55a4adfafdcbe3371f8058180f217ad24ac
Eric <theericlau@gmail.com>
2018-03-11 18:34:41 -0700

    Incrementing reducer completed and working

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -4,10 +4,21 @@
 //copy of current state
 
 function posts(state = [], action) {
-  const { type } = action;
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+      console.log('incrementing likes for ', index);
+      const { index } = action;
+      return [
+        ...state.slice(0, index), // before the one we are updating
+        {...state[index], likes: state[index].likes + 1},
+        ...state.slice(index+1),
+      ]
+    default:
+      return state;
+  }
   // console.log('im the action in post', type);
   // console.log(state, action);
-  return state;
+
 }
 
 export default posts;
