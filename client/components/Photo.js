b03361777f2cc9b8a334ec7d9f19f0aa0df4ffc2
Eric <theericlau@gmail.com>
2018-03-11 18:33:37 -0700

    added onclick feature

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -4,7 +4,7 @@ import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Photo extends Component {
   render() {
-    const { post, i, comments } = this.props;
+    const { post, index, comments, increment } = this.props;
     return (
       <figure className="grid-figure">
         <div className="grid-photo-wrap">
@@ -20,7 +20,7 @@ class Photo extends Component {
         <figcaption>
           <p>{post.caption}</p>
           <div className="control-buttons">
-            <button className="likes">&hearts; {post.likes}</button>
+            <button onClick={ increment.bind(null, index) }className="likes">&hearts; {post.likes}</button>
             <Link to={`/view/${post.code}`} className="button">
               <span className="comment-count">
                 <span className="speech-bubble"> </span>
