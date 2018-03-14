bb894bba1e794217c47aaea01f630e3a136439e2
Eric <theericlau@gmail.com>
2018-03-13 21:44:15 -0700

    Obtained the single photo

--- client/components/Single.js
+++ client/components/Single.js
@@ -3,9 +3,18 @@ import Photo from './Photo';
 
 class Single extends Component {
   render() {
+    console.log(this.props.posts);
+    let post;
+    let { posts, params } = this.props;
+    for (let i =0 ; i < this.props.posts.length; i++ ) {
+      if (posts[i].code === params.postId ){
+        post = posts[i];
+      }
+    }
+    console.log(post);
     return (
         <div className="single-photo">
-          I'm the single
+
         </div>
     );
   }
