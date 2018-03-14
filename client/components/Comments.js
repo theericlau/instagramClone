c9ba05e5cc558c47107dc91996dd2c7fc152ef50
Eric <theericlau@gmail.com>
2018-03-13 23:56:27 -0700

    added commentForm feature

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 const Comment = React.createClass({
+  renderComment(comment, i){
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    )
+  },
+
   render() {
+    let { postComment } = this.props;
     return (
       <div className="comment">
-        Hi
+        {postComment.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form">
+          <input type="text" ref="author" placeholder="author"/>
+          <input type="text" ref="comment" placeholder="comment"/>
+          <input type="submit" hidden/>
+        </form>
       </div>
     )
   }
