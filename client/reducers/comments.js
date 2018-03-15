7c4e9069e7f48571e6e57ca1cf4f36e434e6bc7c
Eric <theericlau@gmail.com>
2018-03-14 21:41:24 -0700

    created separate parts for the comments

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,6 +1,43 @@
-function comments(state = [], action) {
-  console.log('i got triggered comments');
+// function comments(state = [], action) {
+//   switch (action.type) {
+//     case 'ADD_COMMENT':
+//     console.log('add comment',state , action);
+//     let post = {
+//       text: action.comment,
+//       user: action.author
+//     }
+//     return [...state,
+//     state[action.postId].push(post)
+//     ]
+//   default:
+//    return state;
+//   }
+// }
+function comments(state=[], action) {
+  if (typeof action.postId !== 'undefined') {
+    return {
+      ...state,
+      [action.postId]: postComment(state[action.postId], action)
+    }
+  }
+  return state;
+}
 
+
+
+function postComment(state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT':
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      console.log('removing');
+      return state
+    default:
+      return state;
+  }
   return state;
 }
 
