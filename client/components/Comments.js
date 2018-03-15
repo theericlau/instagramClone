02373615b2f4dcccea5863b978822cfe9e3d9b65
Eric <theericlau@gmail.com>
2018-03-14 21:41:02 -0700

    Connected handle remove function

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -7,10 +7,24 @@ const Comment = React.createClass({
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button onClick={this.handleRemove.bind(null, this.props.params.postId, i)} className="remove-comment">&times;</button>
         </p>
       </div>
     )
+  },
+
+  handleRemove(postId, index){
+    this.props.removeComment(postId, index);
+  },
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.refs);
+    const { postId } = this.props.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   },
 
   render() {
@@ -18,7 +32,7 @@ const Comment = React.createClass({
     return (
       <div className="comment">
         {postComment.map(this.renderComment)}
-        <form ref="commentForm" className="comment-form">
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
           <input type="text" ref="author" placeholder="author"/>
           <input type="text" ref="comment" placeholder="comment"/>
           <input type="submit" hidden/>
